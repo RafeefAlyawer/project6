b4996bb523f0416d1109c5e3a888a51cb1b69e05
RAFEEF <RAFEEF@10.0.0.65>
2025-01-31 16:20:40 -0500

    implemented getInf()

--- src/demo1/DemoTest6.java
+++ src/demo1/DemoTest6.java
@@ -3,5 +3,15 @@ package demo1;
 public class DemoTest6 {
  public static void main (String args []) {
 	 System.out.println("JavaIsFun");
+	 System.out.println("Adel");
+	 
+	 int age = 7;
+	 System.out.println(age);
+ }
+ public void getInfo() {
+	 int age = 7;
+	 String name = "Adel";
+	 
+	 System.out.println(name+age);
  }
 }
